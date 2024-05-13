df71de327163c8d7d9dd320340dfa0cf6a437d34
kambam-sathwik <sathwikkambam@gmail.com>
2024-05-13 10:01:18 +0530

    used delete method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function handleFormSubmit(event){
         mail:event.target.email.value
     }
     axios
-    .post("https://crudcrud.com/api/8cce2dc784694234a8e2ff261767e976/appdata",ud)
+    .post("https://crudcrud.com/api/75f5fd46106648c888d2fc15399c8653/appdata",ud)
     .then(res=>displayuser(res.data))
     .catch(err=>console.log(err));
     event.target.username.value='';
@@ -14,15 +14,42 @@ function handleFormSubmit(event){
     event.target.email.value='';
 }
 function displayuser(ud){
-    const list=document.querySelector('ul');
+    if (ud && ud.username && ud.mail && ud.phno) {
+        const list=document.querySelector('ul');
     const nli=document.createElement('li');
     nli.innerHTML=ud.username+" "+ud.mail+" "+ud.phno+
     '<button class="delete-button">Delete</button> <button class="edit-button">Edit</button>';
     list.appendChild(nli);
+    const dbtn=nli.querySelector('.delete-button');
+    dbtn.onclick=()=>{
+        axios
+        .delete(`https://crudcrud.com/api/75f5fd46106648c888d2fc15399c8653/appdata/${ud._id}`)
+        .then(()=>{
+            list.removeChild(nli);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    };
+    const ebtn=nli.querySelector(".edit-button");
+    ebtn.onclick=()=>{
+        axios
+        .delete(`https://crudcrud.com/api/75f5fd46106648c888d2fc15399c8653/appdata/${ud._id}`)
+        .then(()=>{
+            document.querySelector('#username').value=ud.username;
+            document.querySelector('#email').value=ud.mail;
+            document.querySelector('#phone').value=ud.phno;
+            list.removeChild(nli);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    }
 }
 window.addEventListener("DOMContentLoaded",()=>{
     axios
-    .get("https://crudcrud.com/api/8cce2dc784694234a8e2ff261767e976/appdata")
+    .get("https://crudcrud.com/api/75f5fd46106648c888d2fc15399c8653")
     .then((res)=>{
         for(let i=0;i<res.data.length;i++){
             displayuser(res.data[i]);
